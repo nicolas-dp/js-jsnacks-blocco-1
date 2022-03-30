25da484b512c1b29ec3cadfa2cb01ed5903ada5d
Nicolas <wellcomenicolas@gmail.com>
2022-03-30 10:55:04 +0200

    Final Project

--- JSnack-2/main.js
+++ JSnack-2/main.js
@@ -3,14 +3,22 @@ L’utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga. */
 
 
-
 //Chiediamo all'utente la prima parola
-const first_word = prompt("Inserisci il primo numero scelto");
+const first_word = prompt("Inserisci la prima parola scelta");
 
 //Chiediamo all'utente la seconda parola
-const second_word = prompt("Inserisci il secondo numero");
+const second_word = prompt("Inserisci la seconda parola scelta");
 
 //Il software stampa prima la parola più corta e poi quella più lunga
 
+if (first_word.length > second_word.length) {
+    console.log(`E' la parola più CORTA è: ${second_word}`);
+    console.log(`E' la parola più LUNGA è: ${first_word}`);
+} else if (first_word.length < second_word.length) {
+    console.log(`E' la parola più CORTA è: ${first_word}`);
+    console.log(`E' la parola più LUNGA è: ${second_word}`);
+} else {
+    console.log(`Le due parole ${first_word} e ${second_word} hanno la stessa lunghezza`);
+}
 
 
