d7343a055908419dc18f0bf5a6612d68a6dc0cff
Nicolas <wellcomenicolas@gmail.com>
2022-03-30 10:45:27 +0200

    Initial Page JS-SNAK 2

---
+++ JSnack-2/main.js
@@ -0,0 +1,16 @@
+/* SNACK 2 
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga. */
+
+
+
+//Chiediamo all'utente la prima parola
+const first_word = prompt("Inserisci il primo numero scelto");
+
+//Chiediamo all'utente la seconda parola
+const second_word = prompt("Inserisci il secondo numero");
+
+//Il software stampa prima la parola più corta e poi quella più lunga
+
+
+
