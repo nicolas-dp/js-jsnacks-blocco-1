c27ae0fe6527081af5533edd79508603eb7b8c95
Nicolas <wellcomenicolas@gmail.com>
2022-03-30 11:57:58 +0200

    Add project

---
+++ JS-SNACK-3/main.js
@@ -0,0 +1,42 @@
+/* SNACK 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+
+
+//Inizializziamo le variabili
+let primo_numero = parseInt(prompt("Inserisci il primo numero"))
+let secondo_numero = parseInt(prompt("Inserisci il secondo numero"))
+let terzo_numero = parseInt(prompt("Inserisci il terzo numero"))
+let quarto_numero = parseInt(prompt("Inserisci il quarto numero"))
+let quinto_numero = parseInt(prompt("Inserisci il quinto numero"))
+let sesto_numero = parseInt(prompt("Inserisci il sesto numero"))
+let settimo_numero = parseInt(prompt("Inserisci il settimo numero"))
+let ottavo_numero = parseInt(prompt("Inserisci il ottavo numero"))
+let nono_numero = parseInt(prompt("Inserisci il nono numero"))
+let decimo_numero = parseInt(prompt("Inserisci il decimo numero"))
+let somma = (primo_numero + secondo_numero + terzo_numero + quarto_numero + quinto_numero + sesto_numero + settimo_numero + ottavo_numero + nono_numero + decimo_numero);
+
+console.log(somma);
+
+
+for (let i = 1; i <= 10; i++) {
+    const numero = new Array(prompt(`Inserisci il ${i} numero`));
+    console.log(numero);
+
+    somma += numero[i];
+
+    
+}
+
+console.log(somma);
+
+
+
+
+
+
+
+
+
+
