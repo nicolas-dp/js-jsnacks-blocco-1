7dcfe345a9404eabccd832719a5b2bf563e1ced3
Nicolas <wellcomenicolas@gmail.com>
2022-03-30 11:07:06 +0200

    Add control for the word

--- JS-SNACK-2/main.js
+++ JS-SNACK-2/main.js
@@ -9,14 +9,13 @@ const first_word = prompt("Inserisci la prima parola scelta");
 //Chiediamo all'utente la seconda parola
 const second_word = prompt("Inserisci la seconda parola scelta");
 
+console.log(first_word);
+console.log(second_word);
+
 //Il software stampa prima la parola più corta e poi quella più lunga
-
-if (isNaN(first_word) || isNaN(second_word) ) {
-        console.log("Amico non hai inserito le parole correttamente!!!!");
-}
-
-
-if (first_word.length > second_word.length) {
+if (first_word == "" || second_word =="") {
+    console.log("Amico non hai inserito le parole correttamente!!!!");
+} else if (first_word.length > second_word.length) {
     console.log(`E' la parola più CORTA è: ${second_word}`);
     console.log(`E' la parola più LUNGA è: ${first_word}`);
 } else if (first_word.length < second_word.length) {
