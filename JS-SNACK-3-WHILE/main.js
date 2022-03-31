e3a57f29600a69933244cefdadccb7a3c8219f21
Nicolas <wellcomenicolas@gmail.com>
2022-03-31 11:40:36 +0200

    Add cicle WHILE SNACK 3

---
+++ JS-SNACK-3-WHILE/main.js
@@ -0,0 +1,33 @@
+/* SNACK 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+
+let somma, numero_utente;
+let i = 1;
+somma= 0;
+
+while (i <= 10) {
+    numero_utente = parseInt(prompt(`Inerisci il ${i} numero`));
+    somma = somma + numero_utente;
+    i++;
+}
+
+
+
+console.log(somma);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
