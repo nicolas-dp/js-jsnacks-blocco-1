7fd63f7f74484702189368775175e771f656fba9
Nicolas <wellcomenicolas@gmail.com>
2022-03-31 10:23:30 +0200

    Add modify for snack 8

--- JS-SNACK-8/main.js
+++ JS-SNACK-8/main.js
@@ -9,18 +9,24 @@ let numero_richiesto = prompt("Inserisci un numero di 4 cifre");
 /* let result = numero_richiesto.slice(0, 4); */
 let numeriArray = numero_richiesto.split("")
 let somma = 0;
-
+let lunghezza_caratteri = 4
 
 /* console.log(result); */
 console.log(numeriArray);
-for (let i = 0; i < numeriArray.length; i++) {
-    somma += parseInt(numeriArray[i]);
-    
-    
+
+
+
+if (numeriArray.length == 4) {
+    for (let i = 0; i < numeriArray.length; i++) {
+        somma += parseInt(numeriArray[i]);
+        
+        
+    }
+} else {
+    console.log("Hai inserito un numero con più o meno di 4 cifre");
 }
 
-
-console.log(somma);
+console.log(`La somma delle ${numeriArray.length} cifre è: ${somma}`);
 
 
 
