252afd030f6f2df8450016919b44d42f69b11ff6
Nicolas <wellcomenicolas@gmail.com>
2022-03-31 09:55:33 +0200

    Final project - snack 9

---
+++ JS-SNACK-9/main.js
@@ -0,0 +1,26 @@
+/* SNACK 9
+Calcola la somma e la media dei primi 10 numeri.*/
+
+
+let arrayNumbers = new Array();
+let random_number;
+let n_numero = 10;
+let media = 0;
+let somma = 0;
+
+for (let i = 1; i < 100; i++) {
+    arrayNumbers.push(Math.floor(Math.random() * 100) + 1);   
+}
+console.log(arrayNumbers);
+
+for (let i = 0; i < n_numero; i++){
+    somma += arrayNumbers[i]
+    media += somma / 10
+    
+    
+}
+
+console.log(`La somma dei primi 10 numeri è: ${somma} || La media dei primi 10 numeri è: ${media.toFixed(2)}`);
+
+
+
