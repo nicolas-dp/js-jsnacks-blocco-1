25e1eeee9eee5ff62bc22db85670530a99b19e58
Nicolas <wellcomenicolas@gmail.com>
2022-03-31 10:04:42 +0200

    Add modify on project 7

--- JS-SNACK-7/main.js
+++ JS-SNACK-7/main.js
@@ -3,17 +3,23 @@ Stampa le potenze di 2 fino a 1000.*/
 
 
 
-let potenza;
-let arrayPotenza = [];
-let pow_mille;
+let potenza = 0;
+let arrayPotenza = new Array();
+let pow_mille = 1000;
 
-for (let i = 1; i <= 1000; i++) {
+for (let i = 1; i <= 100; i++) {
     potenza = Math.pow(2, i);
-    arrayPotenza.push(potenza);
-
+    
+    if (potenza <= pow_mille) {
+        arrayPotenza.push(potenza); 
+    }
+    
 }
 
 console.log(arrayPotenza);
+
+
+
 
 
 
